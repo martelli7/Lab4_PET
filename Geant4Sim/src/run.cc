a7005fa155c06241b79e3b582b836629c77508ff
Mattia Martelli <147994864+martelli7@users.noreply.github.com>
2023-11-06 10:51:32 +0100

    Update run.cc

--- Geant4Sim/src/run.cc
+++ Geant4Sim/src/run.cc
@@ -4,6 +4,7 @@ MyRunAction::MyRunAction()
 {
 	G4AnalysisManager *man = G4AnalysisManager::Instance();
 
+	//Creation of an Ntuple where storing datas
   	man->CreateNtuple("EnergyDeposition", "Energy deposition");
   	man->CreateNtupleDColumn("fEdepGATE");
   	man->CreateNtupleDColumn("fEdepSPCT");
@@ -15,19 +16,20 @@ MyRunAction::~MyRunAction()
 
 void MyRunAction::BeginOfRunAction(const G4Run* run)
 {	
-  G4AnalysisManager *man = G4AnalysisManager::Instance();
-  G4int runID = run->GetRunID();
+	//Creation of a root file for each run 
+	G4AnalysisManager *man = G4AnalysisManager::Instance();
+  	G4int runID = run->GetRunID();
 
-  std::stringstream strRunID;
-  strRunID << runID;
+  	std::stringstream strRunID;
+  	strRunID << runID;
 
-  man->OpenFile("output"+strRunID.str()+".root");
+  	man->OpenFile("output"+strRunID.str()+".root");
 }
 
 void MyRunAction::EndOfRunAction(const G4Run*)
-{
-    G4AnalysisManager *man = G4AnalysisManager::Instance();
+{	
+    	G4AnalysisManager *man = G4AnalysisManager::Instance();
 
-    man->Write();
-    man->CloseFile();
+    	man->Write();
+    	man->CloseFile();
 }
