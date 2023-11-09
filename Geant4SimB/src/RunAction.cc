4cfad30add81e9ed70abaeda2e5ed58915b45466
Mattia Martelli <m.martelli7@campus.unimib.it>
2023-11-09 10:38:41 +0100

    Changed folder names

--- Geant4SimB/src/RunAction.cc
+++ Geant4SimB/src/RunAction.cc
@@ -24,66 +24,90 @@
 // ********************************************************************
 //
 //
-/// \file B3/B3a/src/EventAction.cc
-/// \brief Implementation of the B3a::EventAction class
+/// \file B3/B3a/src/RunAction.cc
+/// \brief Implementation of the B3a::RunAction class
 
-#include "EventAction.hh"
 #include "RunAction.hh"
+#include "PrimaryGeneratorAction.hh"
 
 #include "G4RunManager.hh"
-#include "G4Event.hh"
-
-#include "G4SDManager.hh"
-#include "G4HCofThisEvent.hh"
-#include "G4THitsMap.hh"
+#include "G4Run.hh"
+#include "G4AccumulableManager.hh"
 #include "G4UnitsTable.hh"
 #include "G4SystemOfUnits.hh"
+
+using namespace B3;
 
 namespace B3a
 {
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-EventAction::EventAction(RunAction* runAction)
- : fRunAction(runAction)
-{}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-void EventAction::BeginOfEventAction(const G4Event* /*evt*/)
-{ }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-void EventAction::EndOfEventAction(const G4Event* evt )
+RunAction::RunAction()
 {
-   //Hits collections
-  //
-  G4HCofThisEvent* HCE = evt->GetHCofThisEvent();
-  if(!HCE) return;
+  // Register accumulable to the accumulable manager
+  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
+  accumulableManager->RegisterAccumulable(fGoodEvents);
+}
 
-   // Get hits collections IDs
-  if (fCollID_cryst < 0) {
-    G4SDManager* SDMan = G4SDManager::GetSDMpointer();
-    fCollID_cryst   = SDMan->GetCollectionID("crystal/edep");
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::BeginOfRunAction(const G4Run* run)
+{
+  G4cout << "### Run " << run->GetRunID() << " start." << G4endl;
+
+  // reset accumulables to their initial values
+  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
+  accumulableManager->Reset();
+
+  //inform the runManager to save random number seed
+  G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::EndOfRunAction(const G4Run* run)
+{
+  G4int nofEvents = run->GetNumberOfEvent();
+  if (nofEvents == 0) return;
+
+  // Merge accumulables
+  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
+  accumulableManager->Merge();
+
+  // Run conditions
+  //  note: There is no primary generator action object for "master"
+  //        run manager for multi-threaded mode.
+  const auto generatorAction = static_cast<const PrimaryGeneratorAction*>(
+    G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
+  G4String partName;
+  if (generatorAction)
+  {
+    G4ParticleDefinition* particle
+      = generatorAction->GetParticleGun()->GetParticleDefinition();
+    partName = particle->GetParticleName();
   }
 
-  //Energy in crystals : identify 'good events'
+  // Print results
   //
-  const G4double eThreshold = 500*keV;
-  G4int nbOfFired = 0;
-
-  auto evtMap = (G4THitsMap<G4double>*)(HCE->GetHC(fCollID_cryst));
-
-  std::map<G4int,G4double*>::iterator itr;
-  for (itr = evtMap->GetMap()->begin(); itr != evtMap->GetMap()->end(); itr++) {
-    ///G4int copyNb  = (itr->first);
-    G4double edep = *(itr->second);
-    if (edep > eThreshold) nbOfFired++;
-    ///G4cout << "\n  cryst" << copyNb << ": " << edep/keV << " keV ";
+  if (IsMaster())
+  {
+    G4cout
+     << G4endl
+     << "--------------------End of Global Run-----------------------"
+     << G4endl
+     << "  The run was " << nofEvents << " events ";
   }
-  if (nbOfFired == 2) fRunAction->CountEvent();
-
+  else
+  {
+    G4cout
+     << G4endl
+     << "--------------------End of Local Run------------------------"
+     << G4endl
+     << "  The run was " << nofEvents << " "<< partName;
+  }
+  G4cout
+     << "; Nb of 'good' e+ annihilations: " << fGoodEvents.GetValue()  << G4endl;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
