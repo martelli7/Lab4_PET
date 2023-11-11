72c322b15e44cb48c1d9039de990135df0eec3a9
Mattia Martelli <147994864+martelli7@users.noreply.github.com>
2023-11-11 11:51:29 +0100

    Update run.cc

--- Geant4SimA/src/run.cc
+++ Geant4SimA/src/run.cc
@@ -15,19 +15,19 @@ MyRunAction::~MyRunAction()
 
 void MyRunAction::BeginOfRunAction(const G4Run* run)
 {	
-  G4AnalysisManager *man = G4AnalysisManager::Instance();
-  G4int runID = run->GetRunID();
+  	G4AnalysisManager *man = G4AnalysisManager::Instance();
+  	G4int runID = run->GetRunID();
 
-  std::stringstream strRunID;
-  strRunID << runID;
+  	std::stringstream strRunID;
+  	strRunID << runID;
 
-  man->OpenFile("output"+strRunID.str()+".root");
+  	man->OpenFile("output"+strRunID.str()+".root");
 }
 
 void MyRunAction::EndOfRunAction(const G4Run*)
 {
-    G4AnalysisManager *man = G4AnalysisManager::Instance();
+    	G4AnalysisManager *man = G4AnalysisManager::Instance();
 
-    man->Write();
-    man->CloseFile();
+    	man->Write();
+    	man->CloseFile();
 }
